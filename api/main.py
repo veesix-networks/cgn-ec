ae1426ff504ca490771ff96808ba6833f2ffa601
BSpendlove <8687668+BSpendlove@users.noreply.github.com>
2025-02-02 10:51:12 +0000

    Initial API key support

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from cgnec_api.dependencies import require_local_api_key
 
 app = FastAPI(
-    title="cgn-ec", description="Flexible solution for your CGNAT logging needs"
+    title="cgn-ec",
+    description="Flexible solution for your CGNAT logging needs",
+    dependencies=[Depends(require_local_api_key)],
 )
 
 from cgnec_api import views  # noqa: E402
