3d124f35dfc92687c4d3ea46ee933c61d78aecd8
BSpendlove <8687668+BSpendlove@users.noreply.github.com>
2025-02-03 09:54:08 +0000

    Fixed session crud to allow dst_ip and dst_port

--- api/cgnec_api/crud/__init__.py
+++ api/cgnec_api/crud/__init__.py
@@ -2,3 +2,4 @@ from cgnec_api.crud.address_mapping import address_mapping
 from cgnec_api.crud.port_block_mapping import port_block_mapping
 from cgnec_api.crud.port_mapping import port_mapping
 from cgnec_api.crud.session_mapping import session_mapping
+from cgnec_api.crud.admin import admin
